9d6d749f6062df9b9fd3dfb654dbc726480e2f2a
Jakob Skjerning <jakob@mentalized.net>
2012-01-19 19:39:57 +0100

    Blank, generated plugin

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_velociraptor do
+  name 'Redmine Velociraptor plugin'
+  author 'Jakob Skjerning'
+  description 'This is a velocity calculation plugin for Redmine'
+  version '0.0.1'
+  # url 'http://example.com/path/to/plugin'
+  author_url 'http://substancelab.com'
+end
