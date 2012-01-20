d33865868bd7fd41555329ad42db29298801779d
Jakob Skjerning <jakob@mentalized.net>
2012-01-20 10:28:24 +0100

    Rename to something less awesome to avoid confusion

--- init.rb
+++ init.rb
@@ -1,9 +1,9 @@
 require 'redmine'
 
-Redmine::Plugin.register :redmine_velociraptor do
-  name 'Redmine Velociraptor plugin'
+Redmine::Plugin.register :redmine_velocity do
+  name 'Redmine Velocity plugin'
   author 'Jakob Skjerning'
-  description 'This is a velocity calculation plugin for Redmine'
+  description 'This is a velocity calculation plugin.'
   version '0.0.1'
   # url 'http://example.com/path/to/plugin'
   author_url 'http://substancelab.com'
